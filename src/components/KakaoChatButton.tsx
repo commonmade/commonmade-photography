adff831eaf442540343ab88d9483c659a471efcd
Feelsgraphy <heojaeyeong@Feelsgraphyui-MacBookPro.local>
2026-03-02 04:25:23 +0900

    Fix Safari HTTP block by upgrading Kakao URL to HTTPS

--- src/components/KakaoChatButton.tsx
+++ src/components/KakaoChatButton.tsx
@@ -9,7 +9,7 @@ export default function KakaoChatButton({ channelId }: KakaoChatButtonProps) {
 
     // 만약 사용자가 "_nTFqX" 대신 "http://pf.kakao.com/_nTFqX" 를 통째로 넣었을 경우를 방어하는 코드
     const cleanId = channelId.includes('/') ? channelId.split('/').pop() : channelId;
-    const chatUrl = `http://pf.kakao.com/${cleanId}/chat`;
+    const chatUrl = `https://pf.kakao.com/${cleanId}/chat`;
 
     return (
         <a
@@ -19,7 +19,7 @@ export default function KakaoChatButton({ channelId }: KakaoChatButtonProps) {
             className="fixed bottom-6 right-6 z-[9999] flex items-center justify-center w-14 h-14 bg-[#FEE500] hover:bg-[#F4DC00] text-[#000000] rounded-full shadow-lg transition-transform hover:scale-110 focus:outline-none"
             aria-label="카카오톡 상담하기"
         >
-            <MessageCircle size={28} className="fill-current" />
+            <MessageCircle size={28} className="fill-current pointer-events-none" />
         </a>
     );
 }
